95d87a6478270476bbc1f5557e1ac6b6e9d4ce34
Dishant <agrawaldishant29@gmail.com>
2022-10-04 19:50:04 +0530

    added getSecureUrl method in MyUtils to make url secure

--- lib/utils/my_utils.dart
+++ lib/utils/my_utils.dart
@@ -1,3 +1,10 @@
 class MyUtils {
   static double roundTo(double value, double precision) => (value * precision).round() / precision;
+
+  static String getSecureUrl(String url) {
+    if(url.startsWith("http:")) {
+      url = url.replaceFirst("http:", "https:");
+    }
+    return url;
+  }
 }
